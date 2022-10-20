369d0fa6e40424d5ef6dc510cae8bd1131778a50
Jay Cho <chotravis87@gmail.com>
2022-10-20 20:40:10 +0900

    add custom font: ROKA

--- App.tsx
+++ App.tsx
@@ -1,7 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from 'react';
+import { useFonts } from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [isInitialized, setInitialized] = useState(false);
+  const [isFontsLoaded] = useFonts({
+    'Roka-Medium': require('./assets/fonts/roka_medium.ttf'),
+    'Roka-Bold': require('./assets/fonts/roka_medium.ttf'),
+  });
+
+  useEffect(() => {
+    const init = async () => {
+      setInitialized(true);
+    };
+    init();
+  }, []);
+
+  if (!isFontsLoaded || !isInitialized) {
+    return <View></View>;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
