5db45d49583c66d52d293ab8771f3780334a0223
Jay Cho <chotravis87@gmail.com>
2022-10-20 22:18:59 +0900

    add alias on module resolver

--- babel.config.js
+++ babel.config.js
@@ -7,6 +7,7 @@ module.exports = function (api) {
         'module-resolver',
         {
           alias: {
+            '@': './src/app',
             palette: './src/palette',
           },
         },
