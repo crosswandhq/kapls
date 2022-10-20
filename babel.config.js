ed0c33434a4b2499ed7a5561a6fe2511768ad650
Jay Cho <chotravis87@gmail.com>
2022-10-20 21:20:04 +0900

    add module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,14 @@
-// eslint-disable-next-line func-names
 module.exports = function (api) {
-  api.cache.forever();
+  api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
       [
         'module-resolver',
         {
-          root: ['./src'],
-          alias: {},
+          alias: {
+            palette: './src/palette',
+          },
         },
       ],
     ],
