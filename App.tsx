43e2a7215e0183adb771b8c0534b435e5a070050
Jay Cho <chotravis87@gmail.com>
2022-10-21 09:51:33 +0900

    prevent screenshot

--- App.tsx
+++ App.tsx
@@ -1,18 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { View } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useFonts } from 'expo-font';
 import { NavigationContainer } from '@react-navigation/native';
 import { RealmContext } from '@/contexts';
 import { base64ToArray, getOrInitEncryptionKey } from '@/utils';
 import { useSettings } from '@/stores';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { usePreventScreenCapture } from 'expo-screen-capture';
+import DropdownAlert from 'react-native-dropdownalert';
+import * as ScreenCapture from 'expo-screen-capture';
 import { RootStackNavigator } from '@/RootStackNavigator';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  usePreventScreenCapture();
+  const alertRef = useRef<DropdownAlert | null>(null);
   const { RealmProvider } = RealmContext;
   const [isInitialized, setInitialized] = useState(false);
   const [isFontsLoaded] = useFonts({
@@ -32,6 +37,16 @@ export default function App() {
   }, []);
 
   useEffect(() => {
+    ScreenCapture.addScreenshotListener(() => {
+      alertRef.current?.alertWithType(
+        'error',
+        '스크린샷 감지',
+        '스크린샷을 감지 하였습니다. 보안을 위해 절대로 앱의 스크린샷을 다른 사람과 공유하지 마세요.'
+      );
+    });
+  }, [alertRef]);
+
+  useEffect(() => {
     if (isFontsLoaded && isInitialized) {
       SplashScreen.hideAsync();
     }
@@ -49,6 +64,7 @@ export default function App() {
           {/* eslint-disable-next-line react/style-prop-object */}
           <StatusBar style='auto' />
         </NavigationContainer>
+        <DropdownAlert ref={alertRef} />
       </GestureHandlerRootView>
     </RealmProvider>
   );
